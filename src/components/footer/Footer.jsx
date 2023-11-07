9c5917c1e8da32893c0f27e3159002109038d43e
rohitAmdahl <rohit_designer@outlook.com>
2023-11-07 23:45:09 +0100

    header and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className="container p-4 mx-auto max-w-md flex flex-col justify-center items-center">
-      <p> &copy; Hundred solution AS.</p>
-      <p> Created by: Rohit Kumar Amdahl {year}</p>
+    <div className="container p-4 mx-auto max-w-md flex flex-col justify-center items-center mt-11">
+      <div className="mt-5">
+        <p> &copy; Hundred solution AS.</p>
+        <p> Created by: Rohit Kumar Amdahl {year}</p>
+      </div>
     </div>
   );
 };
--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/MP-logoTM_V_lock-RGB_color-white.png";
 Link;
 const Navigation = () => {
   return (
-    <div className="mx-auto container max-w-5xl  ">
+    <div className="mx-auto container max-w-5xl m-5 ">
       <Link to="/">
         <img
           src={Logo}
