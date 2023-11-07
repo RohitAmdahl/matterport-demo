9b57d4195ebac4fbec1ba3eb6bdf1edd294759b7
rohitAmdahl <rohit_designer@outlook.com>
2023-11-07 23:44:03 +0100

    data build hard coded

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,23 @@
+import sergo from "../assets/sergo.png";
+import vauban from "../assets/resized.png";
+import domino from "../assets/output-onlinepngtools.png";
 export const dataLinks = [
   {
     id: 0,
     link: "https://matterport.com/discover/space/T4YA5CDttFU",
+    name: "Sergro Cellule 4 TGBT 2",
+    imgUrl: sergo,
   },
   {
     id: 1,
     link: "https://matterport.com/discover/space/yLMJWAYZ98K",
+    name: "Vauban AER 41",
+    imgUrl: vauban,
   },
   {
     id: 2,
     link: "https://matterport.com/discover/space/AGkkqndDnZz",
+    name: "Domino local TGBT",
+    imgUrl: domino,
   },
 ];
