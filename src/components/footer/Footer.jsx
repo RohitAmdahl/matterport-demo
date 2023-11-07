8eda0dbf75bc7c06ec8d75db260f8132f132f471
rohitAmdahl <rohit_designer@outlook.com>
2023-11-08 00:04:32 +0100

    footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="container p-4 mx-auto max-w-md flex flex-col justify-center items-center mt-11">
       <div className="mt-5">
-        <p> &copy; Hundred solution AS.</p>
+        <p> &copy; Hundred solution AS, All rights reserved.</p>
         <p> Created by: Rohit Kumar Amdahl {year}</p>
       </div>
     </div>
