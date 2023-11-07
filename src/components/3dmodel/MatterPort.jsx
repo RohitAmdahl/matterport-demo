4f6d9461cd7ba6a58c1b9d7f7bac0713c6e41f99
rohitAmdahl <rohit_designer@outlook.com>
2023-11-07 23:50:35 +0100

    remove console log

--- src/components/3dmodel/MatterPort.jsx
+++ src/components/3dmodel/MatterPort.jsx
@@ -3,24 +3,22 @@ import { dataLinks } from "../../data/data";
 const MatterPort = () => {
   const { id } = useParams();
   const selectedItem = dataLinks.find((item) => {
-    console.log(item);
     return item.id === item.id;
   });
   if (!selectedItem) {
     return <div> Item not found </div>;
   }
-  console.log(selectedItem);
-  console.log(id);
-  const { name, imgUrl, link } = selectedItem;
-  console.log(name);
+
+  const { link } = selectedItem;
+
   return (
     <div>
       <iframe
         title="Embedded Content"
-        width="100%" // Set the width to 100% of the parent container
-        height="600px" // Set the height to 500 pixels
-        src={link} // URL of the content you want to embed
-        allowFullScreen // Optional: Allow fullscreen mode
+        width="100%"
+        height="600px"
+        src={link}
+        allowFullScreen
       ></iframe>
     </div>
   );
